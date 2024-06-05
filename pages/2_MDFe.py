46f4cabf35394c3fca0de3fc8a009d5257547dcd
name <jesuanekafer2@gmail.com>
2024-06-05 08:36:12 -0300

    removendo função do grafico errada em MDFe e CTe

--- pages/2_MDFe.py
+++ pages/2_MDFe.py
@@ -22,48 +22,49 @@ with col1:
 with col2:
     end_date = st.date_input("Selecione a data final")
 
-def get_data_by_column(table_name, column_name):
-    try:
-        response = requests.get(f"http://127.0.0.1:8501/get-list/{table_name}/{column_name}")
-        response.raise_for_status()
-        data = response.json()
-        return pd.DataFrame(data)
-    except requests.exceptions.HTTPError as err:
-        st.error(f"Erro HTTP: {err}")
-    except requests.exceptions.RequestException as err:
-        st.error(f"Erro ao fazer a requisição: {err}")
-    except Exception as err:
-        st.error(f"Erro: {err}")
-    return pd.DataFrame()
-
-table_name = "dbo.T_MDFE"
-column_name = "dbo.MDF_DATAEMISSAO"
-df = get_data_by_column(table_name, column_name)
-
-# Verificar se há dados
-if df.empty:
-    st.write("Nenhum dado encontrado.")
-else:
-    # Converter a coluna de data para datetime
-    df['dbo.MDF_DATAEMISSAO'] = pd.to_datetime(df['dbo.MDF_DATAEMISSAO'], format='%Y/%m/%d %H:%M:%S')
-
-    # Filtrar dados por data selecionada
-    filtered_df = df[(df['dbo.MDF_DATAEMISSAO'] >= pd.to_datetime(start_date)) & (df['dbo.MDF_DATAEMISSAO'] <= pd.to_datetime(end_date))]
-
-    # Verificar se há dados no período selecionado
-    if filtered_df.empty:
-        st.write("Nenhum dado encontrado para o período selecionado.")
-    else:
-        # Gráfico
-        chart = alt.Chart(filtered_df).mark_bar().encode(
-            x='dbo.MDF_DATAEMISSAO:T',
-            y=alt.Y('count()', title='Contagem'),
-            tooltip=['dbo.MDF_DATAEMISSAO', 'count()']
-        ).properties(
-            title='Notas Emitidas por Período',
-            width=800,
-            height=400
-        ).interactive()
-
-        st.altair_chart(chart, use_container_width=True)
-
+#def get_data_by_column(table_name, column_name):
+#    try:
+#        response = requests.get(f"http://127.0.0.1:8000/get-list/{table_name}/{column_name}")
+#        response.raise_for_status()
+#        data = response.json()
+#        return pd.DataFrame(data)
+#    except requests.exceptions.HTTPError as err:
+#        st.error(f"Erro HTTP: {err}")
+#    except requests.exceptions.RequestException as err:
+#        st.error(f"Erro ao fazer a requisição: {err}")
+#    except Exception as err:
+#        st.error(f"Erro: {err}")
+#    return pd.DataFrame()
+#
+#table_name = "dbo.T_MDFE"
+#column_name = "dbo.MDF_DATAEMISSAO"
+#df = get_data_by_column(table_name, column_name)
+#
+## Verificar se há dados
+#if df.empty:
+#    st.write("Nenhum dado encontrado.")
+#else:
+#    # Converter a coluna de data para datetime
+#    df['dbo.MDF_DATAEMISSAO'] = pd.to_datetime(df['dbo.MDF_DATAEMISSAO'], format='%Y/%m/%d %H:%M:%S')
+#
+#    # Filtrar dados por data selecionada
+#    filtered_df = df[(df['dbo.MDF_DATAEMISSAO'] >= pd.to_datetime(start_date)) & (df['dbo.MDF_DATAEMISSAO'] <= pd.to_datetime(end_date))]
+#
+#    # Verificar se há dados no período selecionado
+#    if filtered_df.empty:
+#        st.write("Nenhum dado encontrado para o período selecionado.")
+#    else:
+#        # Gráfico
+#        chart = alt.Chart(filtered_df).mark_bar().encode(
+#            x='dbo.MDF_DATAEMISSAO:T',
+#            y=alt.Y('count()', title='Contagem'),
+#            tooltip=['dbo.MDF_DATAEMISSAO', 'count()']
+#        ).properties(
+#            title='Notas Emitidas por Período',
+#            width=800,
+#            height=400
+#        ).interactive()
+#
+#        st.altair_chart(chart, use_container_width=True)
+#
+#
--- pages/3_CTe.py
+++ pages/3_CTe.py
@@ -22,48 +22,49 @@ with col1:
 with col2:
     end_date = st.date_input("Selecione a data final")
 
-def get_data_by_column(table_name, column_name):
-    try:
-        response = requests.get(f"http://127.0.0.1:8000/get-list/{table_name}/{column_name}")
-        response.raise_for_status()
-        data = response.json()
-        return pd.DataFrame(data)
-    except requests.exceptions.HTTPError as err:
-        st.error(f"Erro HTTP: {err}")
-    except requests.exceptions.RequestException as err:
-        st.error(f"Erro ao fazer a requisição: {err}")
-    except Exception as err:
-        st.error(f"Erro: {err}")
-    return pd.DataFrame()
-
-table_name = "T_CTE"
-column_name = "CON_DATAHORAEMISSAO"
-df = get_data_by_column(table_name, column_name)
-
-# Verificar se há dados
-if df.empty:
-    st.write("Nenhum dado encontrado.")
-else:
-    # Converter a coluna de data para datetime
-    df['CON_DATAHORAEMISSAO'] = pd.to_datetime(df['CON_DATAHORAEMISSAO'])
-
-    # Filtrar dados por data selecionada
-    filtered_df = df[(df['CON_DATAHORAEMISSAO'] >= pd.to_datetime(start_date)) & (df['CON_DATAHORAEMISSAO'] <= pd.to_datetime(end_date))]
-
-    # Verificar se há dados no período selecionado
-    if filtered_df.empty:
-        st.write("Nenhum dado encontrado para o período selecionado.")
-    else:
-        # Criar gráfico de barras usando Altair
-        chart = alt.Chart(filtered_df).mark_bar().encode(
-            x='CON_DATAHORAEMISSAO:T',
-            y='count:Q',
-            tooltip=['CON_DATAHORAEMISSAO', 'count']
-        ).properties(
-            title='Notas Emitidas por Período',
-            width=800,
-            height=400
-        ).interactive()
-
-        # Exibir gráfico
-        st.altair_chart(chart, use_container_width=True)
+#def get_data_by_column(table_name, column_name):
+#    try:
+#        response = requests.get(f"http://127.0.0.1:8000/get-list/{table_name}/{column_name}")
+#        response.raise_for_status()
+#        data = response.json()
+#        return pd.DataFrame(data)
+#    except requests.exceptions.HTTPError as err:
+#        st.error(f"Erro HTTP: {err}")
+#    except requests.exceptions.RequestException as err:
+#        st.error(f"Erro ao fazer a requisição: {err}")
+#    except Exception as err:
+#        st.error(f"Erro: {err}")
+#    return pd.DataFrame()
+#
+#table_name = "T_CTE"
+#column_name = "CON_DATAHORAEMISSAO"
+#df = get_data_by_column(table_name, column_name)
+#
+## Verificar se há dados
+#if df.empty:
+#    st.write("Nenhum dado encontrado.")
+#else:
+#    # Converter a coluna de data para datetime
+#    df['CON_DATAHORAEMISSAO'] = pd.to_datetime(df['CON_DATAHORAEMISSAO'])
+#
+#    # Filtrar dados por data selecionada
+#    filtered_df = df[(df['CON_DATAHORAEMISSAO'] >= pd.to_datetime(start_date)) & (df['CON_DATAHORAEMISSAO'] <= pd.to_datetime(end_date))]
+#
+#    # Verificar se há dados no período selecionado
+#    if filtered_df.empty:
+#        st.write("Nenhum dado encontrado para o período selecionado.")
+#    else:
+#        # Criar gráfico de barras usando Altair
+#        chart = alt.Chart(filtered_df).mark_bar().encode(
+#            x='CON_DATAHORAEMISSAO:T',
+#            y='count:Q',
+#            tooltip=['CON_DATAHORAEMISSAO', 'count']
+#        ).properties(
+#            title='Notas Emitidas por Período',
+#            width=800,
+#            height=400
+#        ).interactive()
+#
+#        # Exibir gráfico
+#        st.altair_chart(chart, use_container_width=True)
+#
