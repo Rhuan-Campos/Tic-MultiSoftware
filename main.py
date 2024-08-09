382df73eec09f9b37d1c26bcab8aac409b0b1825
slowmotion7 <alessandroluigicorrea@gmail.com>
2024-08-09 15:53:48 -0300

    resolvido bug do main.py

--- main.py
+++ main.py
@@ -5,53 +5,48 @@ import pandas as pd
 import uvicorn
 import os
 
-App = FastAPI()
+app = FastAPI() 
 
-
-App.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-csv_path = r"C:\Users\Softex\Desktop\Nova pasta\CTE - Copia.csv"
-
+csv_path = r"C:\\Users\\Softex\\Desktop\\banco de daods\\CTE - Copia.csv"
 
 if os.path.exists(csv_path):
     df = pd.read_csv(csv_path)
 else:
     raise FileNotFoundError(f"O arquivo {csv_path} não foi encontrado.")
 
+@app.get("/")
+def read_root():
+    return {"FastApi funcionando"}
 
 class DataFrameData(BaseModel):
     data: list
 
-
-@App.get("/columns")
+@app.get("/columns")
 def get_columns():
     return {"columns": df.columns.tolist()}
 
-
-@App.get("/data/{column}", response_model=DataFrameData)
+@app.get("/data/{column}", response_model=DataFrameData)
 def get_data(column: str):
     if column not in df.columns:
         raise HTTPException(status_code=404, detail="Column not found")
     data = df[column].dropna().tolist()
     return {"data": data}
 
-
-@App.get("/unique_counts/{column}")
+@app.get("/unique_counts/{column}")
 def get_unique_counts(column: str):
     if column not in df.columns:
         raise HTTPException(status_code=404, detail="Column not found")
     unique_counts = df[column].value_counts().to_dict()
     return {"unique_counts": unique_counts}
 
-
-# Endpoint para obter a média dos valores de uma coluna
-@App.get("/mean/{column}")
+@app.get("/mean/{column}")
 def get_mean(column: str):
     if column not in df.columns:
         raise HTTPException(status_code=404, detail="Column not found")
@@ -60,8 +55,7 @@ def get_mean(column: str):
     mean_value = df[column].mean()
     return {"mean": mean_value}
 
-# Endpoint para obter a mediana dos valores de uma coluna
-@App.get("/median/{column}")
+@app.get("/median/{column}")
 def get_median(column: str):
     if column not in df.columns:
         raise HTTPException(status_code=404, detail="Column not found")
@@ -70,8 +64,7 @@ def get_median(column: str):
     median_value = df[column].median()
     return {"median": median_value}
 
-# Endpoint para obter o desvio padrão dos valores de uma coluna
-@App.get("/std_dev/{column}")
+@app.get("/std_dev/{column}")
 def get_std_dev(column: str):
     if column not in df.columns:
         raise HTTPException(status_code=404, detail="Column not found")
@@ -80,8 +73,7 @@ def get_std_dev(column: str):
     std_dev_value = df[column].std()
     return {"std_dev": std_dev_value}
 
-# Endpoint para obter os valores mínimos e máximos de uma coluna
-@App.get("/min_max/{column}")
+@app.get("/min_max/{column}")
 def get_min_max(column: str):
     if column not in df.columns:
         raise HTTPException(status_code=404, detail="Column not found")
@@ -91,14 +83,12 @@ def get_min_max(column: str):
     max_value = df[column].max()
     return {"min": min_value, "max": max_value}
 
-# Endpoint para obter o resumo estatístico das colunas numéricas
-@App.get("/summary")
+@app.get("/summary")
 def get_summary():
     summary = df.describe(include='all').to_dict()
     return {"summary": summary}
 
-# Endpoint para obter a correlação entre duas colunas numéricas
-@App.get("/correlation/{column1}/{column2}")
+@app.get("/correlation/{column1}/{column2}")
 def get_correlation(column1: str, column2: str):
     if column1 not in df.columns or column2 not in df.columns:
         raise HTTPException(status_code=404, detail="Column(s) not found")
@@ -107,6 +97,5 @@ def get_correlation(column1: str, column2: str):
     correlation = df[column1].corr(df[column2])
     return {"correlation": correlation}
 
-
 if __name__ == "__main__":
-    uvicorn.run(App, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)  
